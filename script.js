5ac411c4d95d0c44810ed928f6f30ef2ccb02186
Farhana <farhanafatema@hotmail.com>
2020-07-04 01:31:12 +0100

    completed level 400, need to debug, improve slideshow

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 //You can edit ALL of the code here
-//const allEpisodes = getAllEpisodes();
+
 let allEpisodes;
-
-
+const allShows = getAllShows();
 const rootElem = document.getElementById("root");
 const flexContainer = document.querySelector(".flex-container");
 const searchBar = document.getElementById("search-word");
 const displayParagraph = document.querySelector(".search-container p");
 const dropDownEl = document.getElementById("episode-select");
-
+const showsDropDownEl = document.getElementById("show-select");
 
 function setup() {
-  
 
-  fetch('https://api.tvmaze.com/shows/527/episodes')
-  .then(response => response.json())
-  .then(data => {
-    
-    allEpisodes = data;  
-    makePageForEpisodes(allEpisodes)
-    searchBar.addEventListener('keyup',doLiveSearch)
-    createDropDownList(allEpisodes);
-    dropDownEl.addEventListener('change',selectEpisode);
+  showSlides(slideIndex);
+  createShowList(allShows);
+  showsDropDownEl.addEventListener('change', selectShow);
 }
-  );
 
-}
+
 
          /*Function for fromatting episode title*/
 
@@ -68,31 +59,73 @@ function doLiveSearch(){
 
 }
 
-           /*function to create the dropdown list */
+        /*function to create the dropdown list for shows */
+
+function createShowList(showsList){
+
+  showsList.forEach(show =>{
+    const optEl = document.createElement("option");
+    optEl.value = show.id;
+    optEl.textContent = show.name;
+    showsDropDownEl.appendChild(optEl);
+
+  })
+}        
+
+          /*function to select a show */
+function selectShow(){
+  let SHOW_ID = event.target.value;
+  const episodeSelectEl = document.querySelector("#all-episodes");
+  console.log(episodeSelectEl);
+  
+  fetch(`https://api.tvmaze.com/shows/${SHOW_ID}/episodes`)
+  .then(response => response.json())
+  .then(data => {
+    allEpisodes = data;  
+    //console.log(allEpisodes.names);
+    makePageForEpisodes(allEpisodes);
+    searchBar.addEventListener('keyup',doLiveSearch);
+    createDropDownList(allEpisodes);
+    dropDownEl.addEventListener('change',selectEpisode);
+  })
+  .catch(err => console.log(err));
+
+} 
+
+           /*function to create the dropdown list for episodes */
 function createDropDownList(episodeList){
+  let prevEpisodes = document.querySelectorAll("#episode-select option");
+  for(let i = 1; i < prevEpisodes.length; i++){
+    dropDownEl.removeChild(prevEpisodes[i]);
+  }
+  console.log(prevEpisodes);
 
   episodeList.forEach(episode => {
-    const optionEl = document.createElement("option");
+    let optionEl = document.createElement("option");
     optionEl.value = episode.name;
-    let s = episode.season;
-    let e = episode.number;
-    let sE = addZeros(s,e);
+    let seasonNumber = episode.season;
+    let episodeNumber = episode.number;
+    let sE = addZeros(seasonNumber,episodeNumber);
+    optionEl.textContent = " ";
     optionEl.textContent = sE + " - " + episode.name;
+    //console.log(optionEl);
     dropDownEl.appendChild(optionEl);
+    //console.log(sE + " - " + episode.name);
 
   })
  
 
-}   
+}  
+
+         
 
             /*function to select an episode */
 function selectEpisode(){
+
   const selected = event.target.value;
   const episodeCards = document.querySelectorAll(".episodes-card");
   const episodeTitles = document.querySelectorAll("h3");
-
   
-
   for(let i = 0; i < episodeCards.length; i++){
     if(selected != "showAll"){
 
@@ -124,10 +157,10 @@ function createEpisodeCards(episode){
     let episodeImgEl = document.createElement("img");
 
     let episodeDecsriptionEl = document.createElement("p");
-    let s = episode.season;
-    let e = episode.number;
+    let seasonNumber = episode.season;
+    let episodeNumber = episode.number;
     //console.log(s,e);
-    let sE = addZeros(s,e);
+    let sE = addZeros(seasonNumber,episodeNumber);
 
     hEl.innerText = episode.name +" - "+ sE;
     // console.log(hEl);
@@ -141,29 +174,71 @@ function createEpisodeCards(episode){
 }
 
 
-/*function to display all the episodes*/
+       /*function to display all the episodes*/
 
 function makePageForEpisodes(episodeList) {
 
-  flexContainer.innerHTML = " ";
-  
+  flexContainer.innerHTML = " "; 
  
-
-  episodeList.forEach(function(episode){
+  episodeList.forEach(episode => {
 
     createEpisodeCards(episode);
   });
-
-  // searchBar.addEventListener('keyup', doLiveSearch);
-
-  
-
-  
-
-  
-
-    
 }
+
+     /* creatin a slide show */
+
+let slideIndex = 1;
+let timer = null;
+
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n==undefined){
+      n = ++slideIndex
+    }
+
+  if (n > slides.length) {
+      slideIndex = 1
+    }
+
+  if (n < 1) {
+        slideIndex = slides.length
+    }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  timer = setTimeout(showSlides, 2000);
+} 
+
+
+
+
+
+
+
+
 
 window.onload = setup;
 
